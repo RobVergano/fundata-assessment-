ec42fb83dbdd8114ffd421f54a565656f8146f97
RobVergano <10097802+RobVergano@users.noreply.github.com>
2022-10-23 20:17:30 +0100

    Create logzero.py

---
+++ log zero exercise/logzero.py
@@ -0,0 +1,8 @@
+#logzero.py
+#Program to demonstrate why the log of 0 is undefined.
+
+import math
+
+print(-math.log(0,2))
+
+# The math module gives an error since it does not recognize the log of 0.
